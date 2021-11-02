60e53d1cde4f9fb594adcea82799245ac87c9d67
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-11-02 18:21:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ const admCategories = require('./admin/categories/categories');
 
 const app = express();
 
+/* View Engine and static files configuration */
 app.set('view engine', 'ejs');
 app.use(express.static('src'));
 
+/* Connection of databases */
 connection
   .authenticate()
   .then()
   .catch(msgErro => console.log(msgErro));
 
+/* Models admin */
 function Admin() {
   app.use('/admin', admProducts);
   app.use('/admin', admCategories);
 }
 
+/* Homepage */
 app.get('/', (req, res) => {
   const nameOfPage = null;
   res.render('index', {
@@ -25,6 +29,7 @@ app.get('/', (req, res) => {
   });
 });
 
+/* Server */
 app.listen(80, (erro) => {
   if (erro) console.log(`Ocorreu um erro ao conectar o server: \n${erro}`);
 });
