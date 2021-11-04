53499e593342597685aee9735fffba1467df4046
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-11-04 15:12:51 -0300

    ajustes de timezone

--- databases/connection.js
+++ databases/connection.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('labeca', 'root', 'lucas2012', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '-03:00',
 });
 
 module.exports = connection;
