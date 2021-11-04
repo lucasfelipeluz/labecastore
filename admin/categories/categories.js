a2e393c838057b60deace751f480acb54fb663af
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-11-03 22:25:57 -0300

    tabela categoria criada

---
+++ admin/categories/categories.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const connection = require('../../databases/connection');
+
+const Categories = connection.define('categories', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Categories;
