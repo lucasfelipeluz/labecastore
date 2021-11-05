c9af21fc6df16ca73ed2b762a6ae0c47077d896d
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2021-11-05 11:06:23 -0300

    arquivo de utilidades pronto

---
+++ admin/products/products.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const connection = require('../../databases/connection');
+const Categories = require('../categories/categories');
+
+const Products = connection.define('products', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  inventoryP: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  inventoryM: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  inventoryG: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  inventoryTotal: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
