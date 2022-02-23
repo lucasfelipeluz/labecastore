e864dd0c31334f8519dd8e7fc30b6291f05d5e03
lucasfelipeluz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-02-23 14:27:22 -0300

    refatoracao no iniciador do server

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use('/', router);
 
 /* Iniciando Server */
 app.listen(80, (erro) => {
-  if (!erro) console.log('Servidor rodando!')
+  if (erro) console.log('Erro ao iniciar o servidor!')
 });
 
 module.exports = app;
