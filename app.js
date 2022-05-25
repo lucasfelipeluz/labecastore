816601646da61a3f7cca84710ffc4dd2fde87744
lucasfelipeluz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-05-25 18:55:34 -0300

    Ajustes

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const yaml = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
 
 const router = require('./routes/routes');
-// const swaggerDocs = require('./docs/swagger.json')
-const swaggerYAML = yaml.load('./docs/swagger.yaml')
 
 const app = express();
 
@@ -16,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Swagger
+const swaggerYAML = yaml.load('./docs/swagger.yaml')
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerYAML))
 
 // Usando Rotas
