ee3a616fba254908d901d6eaeddfba7b3bd44403
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-03 18:55:47 -0300

    fix(prettier): ajustes na Indentação

--- databases/connection.js
+++ databases/connection.js
@@ -1,7 +1,7 @@
 /*  Método responsável por conectar ao Banco de dados
     MySQL. */
-const database = require('knex')({
-  client: 'mysql2',
+const database = require("knex")({
+  client: "mysql2",
   connection: {
     host: process.env.db_host,
     user: process.env.db_user,
