96138d2a0e5ce2f41930e9f69dfa466dab54ca32
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-03 18:54:06 -0300

    fix: integração Controller com Model de Categorias

--- controllers/admin/CategoriesController.js
+++ controllers/admin/CategoriesController.js
@@ -1,120 +1,144 @@
-const slugify = require('slugify');
-const Categories = require('../../models/admin/Categories');
-const Responses = require('../../utils/Responses')
+const slugify = require("slugify");
+const Categories = require("../../models/Categories");
+const Responses = require("../../utils/Responses");
+const Database = require("../../databases/database");
 
 // Classe responsável pelo serviços da rota admin/categories
 class CategoriesController {
-
   // Retorna todas as categorias
   async index(req, res) {
-    const responseFindAllCategories = await Categories.findAll()
+    try {
+      const connectionOption = Database.getConnectionOption();
 
-    const helpRoutes = [
-      'POST/admin/categories',
-      'PUT/admin/categories/:id', 
-      'DEL/admin/categories/:id'
-    ]
+      const responseFindAllCategories = await Categories(
+        connectionOption
+      ).findAll();
 
-    // No Content
-    if (responseFindAllCategories.status === null) {
-      return Responses.noContent(res)
+      const helpRoutes = [
+        "POST/admin/categories",
+        "PUT/admin/categories/:id",
+        "DEL/admin/categories/:id",
+      ];
+
+      // No Content
+      if (responseFindAllCategories.status === null) {
+        return Responses.noContent(res);
+      }
+
+      // Success
+      return Responses.success(res, responseFindAllCategories, {
+        helpRoutes,
+      });
+    } catch (error) {
+      console.log(error);
+      return Responses.internalServerError(res, [], {}, error);
     }
-
-    // Success
-    if (responseFindAllCategories.status === true) {
-      return Responses.success(res, responseFindAllCategories.data, {helpRoutes})
-    }
-
-    // Internal Server Error
-    return Responses.internalServerError(res)
   }
 
   // Criação de Categorias
   async create(req, res) {
-    const { id, name } = req.body;
+    try {
+      const connectionOption = Database.getConnectionOption();
 
-    // Bad Request
-    if (name === undefined) {
-      const msgError = "Name não esta sendo enviado!";
-      return Responses.badRequest(res, {} , {}, msgError);
+      const { id, name } = req.body;
+
+      // Bad Request
+      if (name === undefined) {
+        const msgError = "Name não esta sendo enviado!";
+        return Responses.badRequest(res, {}, {}, msgError);
+      }
+      if (name === "" || name === null) {
+        return Responses.badRequest(
+          res,
+          {},
+          {},
+          "O nome da categoria é obrigatório."
+        );
+      }
+
+      const responseCreateCategory = await Categories(connectionOption).create({
+        name,
+        slug: slugify(name).toLowerCase(),
+      });
+
+      // Success
+      return Responses.created(res, responseCreateCategory);
+    } catch (error) {
+      console.log(error);
+      return Responses.internalServerError(res, [], {}, error);
     }
-    if (name === '' || name === null) {
-      return Responses.badRequest(res, {}, {}, 'O nome da categoria é obrigatório.')
-    }
-
-    // Preparando dados para inserção
-    const data = {
-      id,
-      name,
-      slug: slugify(name).toLowerCase(),
-    };
-
-    const responseCreateCategory = await Categories.insertData(data);
-
-
-    // Success
-    if (responseCreateCategory.status) {
-      return Responses.created(res, responseCreateCategory.data)
-    }
-
-    // Internal Server Error
-    return Responses.internalServerError(res)
   }
 
   // Atualizar Categoria
   async update(req, res) {
-    const { name } = req.body;
-    const { id } = req.params;
+    try {
+      const connectionOption = Database.getConnectionOption();
 
-    // Bad Request
-    if (name === undefined || id === undefined) {
-      return Responses.badRequest(res, {}, {}, 'Name ou Id não estão sendo enviados!');
+      const { name } = req.body;
+      const { id } = req.params;
+
+      // Bad Request
+      if (name === undefined || id === undefined) {
+        return Responses.badRequest(
+          res,
+          {},
+          {},
+          "Name ou Id não estão sendo enviados!"
+        );
+      }
+
+      if (name === "" || name === null) {
+        return Responses.notAcceptable(
+          res,
+          {},
+          {},
+          "O nome da categoria é obrigatório."
+        );
+      }
+
+      await Categories(connectionOption).update(
+        {
+          name,
+          slug: slugify(name).toLowerCase(),
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+
+      return Responses.success(res);
+    } catch (error) {
+      console.log(error);
+      return Responses.internalServerError(res, [], {}, error);
     }
-    if (name === '' || name === null) {
-      return Responses.notAcceptable(res, {}, {}, 'O nome da categoria é obrigatório.')
-    }
-
-    const data = {
-      name,
-      slug: slugify(name).toLowerCase(),
-    };
-
-    const responseUpdateData = await Categories.updateData(id, data);
-
-    // Bad Request
-    if (responseUpdateData.status === null) {
-      return Responses.badRequest(res, {}, {}, 'Categoria não encontrada')
-    }
-
-    // Success
-    if (responseUpdateData.status) {
-      return Responses.success(res, responseUpdateData.data)
-    }
-
-    // Internal Server Error
-    return Responses.internalServerError(res)
   }
 
   // Deletando Categoria
   async delete(req, res) {
-    const { id } = req.params;
+    try {
+      const connectionOption = Database.getConnectionOption();
 
-    const response = await Categories.deleteData(id);
-  
-    // Bad Request
-    if (response.status === null) {
-      return Responses.badRequest(res, {}, {}, 'Categoria não encontrada')
+      const { id } = req.params;
+
+      const response = await Categories(connectionOption).destroy({
+        where: {
+          id,
+        },
+      });
+
+      // Bad Request
+      if (response.status === null) {
+        return Responses.badRequest(res, {}, {}, "Categoria não encontrada");
+      }
+
+      return Responses.success(res, response.data);
+    } catch (error) {
+      console.log(error);
+      return Responses.internalServerError(res, [], {}, error);
     }
-
-    // Success
-    if (response.status) {
-      return Responses.success(res, response.data)
-    }
-
-    // Internal Server Error
-    return Responses.internalServerError(res)
   }
-
 }
 
 module.exports = new CategoriesController();
