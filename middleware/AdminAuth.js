55ebc4e3ffb53dcdb8659bd971fbb583684497ef
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-17 12:14:38 -0300

    chore: ajustes no middleware

--- middleware/AdminAuth.js
+++ middleware/AdminAuth.js
@@ -1,27 +1,29 @@
-const jwt = require('jsonwebtoken');
-const Responses = require('../utils/Responses');
+const jwt = require("jsonwebtoken");
+const Responses = require("../utils/Responses");
 
 const secretKey = process.env.secret_key;
 
 module.exports = (req, res, next) => {
-  const authToken = req.headers['authorization']
+  const authToken = req.headers["authorization"];
 
-  if (process.env.authStatus === 'true'){
-
+  if (process.env.authStatus === "true") {
     if (authToken != undefined) {
-
-  
       jwt.verify(authToken, secretKey, {}, (error, decoded) => {
         if (error) {
-          return Responses.forbidden(res, [], {}, 'Você não tem permissão para acessar essa rota!')
+          return Responses.forbidden(
+            res,
+            "Você não tem permissão para acessar essa rota!",
+            [],
+            {}
+          );
         }
         if (decoded) {
-          next()
+          console.log(decoded);
+          next();
         }
-      })
-  
-  
-    } else return Responses.unauthenticated(res, [], {}, 'Você não está logado!')
-
+      });
+    } else {
+      return Responses.unauthenticated(res, "Você não está logado!", [], {});
+    }
   } else next();
-}
+};
