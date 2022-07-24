cf8eb9d1ac8e251ae067c510a2a8dec4209dc810
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-24 14:41:14 -0300

    feat: ajustes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Importando variáveis de ambiente */
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const yaml = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const yaml = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 
-const router = require('./routes/routes');
+const router = require("./routes/routes");
 
 const app = express();
 
@@ -14,21 +14,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Swagger
-if (process.env.server_mode == 'dev'){
-  const swaggerYAML = yaml.load('./docs/swagger.yaml')
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerYAML))
+if (process.env.server_mode == "dev") {
+  const swaggerYAML = yaml.load("./docs/swagger.yaml");
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerYAML));
 }
 
 // Usando Rotas
-app.use('/', router);
+app.use("/", router);
 
 /* Iniciando Server */
 const port = process.env.PORT || 8989;
-app.listen(port, erro => {
-  if (process.env.server_mode == 'dev'){
-    if (erro) console.log('Erro ao iniciar o servidor!')
-    else {console.log(`Server running in port: ${port}`)}
+app.listen(port, (erro) => {
+  if (process.env.server_mode == "dev") {
+    if (erro) console.log("Erro ao iniciar o servidor!");
+    else {
+      console.log(`Server running in port: ${port}`);
+    }
   }
+});
+
+console.log({
+  server_mode: process.env.server_mode,
+  port,
 });
 
 module.exports = app;
