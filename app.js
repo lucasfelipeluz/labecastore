26707806e4c6884f587fb9835ad54cab1b487896
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-07-24 19:06:38 -0300

    Chore/AjustesNoRuntime (#34)
    - feat: ajustes
    - chore: ajustes no modo de desenvolvimento

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Importando variáveis de ambiente */
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
-const yaml = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const yaml = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 
-const router = require('./routes/routes');
+const router = require("./routes/routes");
 
 const app = express();
 
@@ -14,20 +14,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Swagger
-if (process.env.server_mode == 'dev'){
-  const swaggerYAML = yaml.load('./docs/swagger.yaml')
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerYAML))
+if (process.env.server_mode == "dev") {
+  const swaggerYAML = yaml.load("./docs/swagger.yaml");
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerYAML));
 }
 
 // Usando Rotas
-app.use('/', router);
+app.use("/", router);
 
 /* Iniciando Server */
-const port = process.env.PORT || 8989;
-app.listen(port, erro => {
-  if (process.env.server_mode == 'dev'){
-    if (erro) console.log('Erro ao iniciar o servidor!')
-    else {console.log(`Server running in port: ${port}`)}
+const port = process.env.port || 8989;
+app.listen(port, (erro) => {
+  if (process.env.server_mode == "dev") {
+    if (erro) console.log("Erro ao iniciar o servidor!");
+    else {
+      console.log(`Development server running in port: ${port}`);
+    }
+  } else {
+    if (erro) console.log("Erro ao iniciar o servidor!");
+    else {
+      console.log(`Server running in port: ${port}`);
+    }
   }
 });
 
--- labeca.sql
+++ labeca.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `labeca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `labeca`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: labeca
@@ -27,17 +29,26 @@ CREATE TABLE `categories` (
   `name` varchar(25) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
   `slug` varchar(50) NOT NULL DEFAULT '0',
   `createdAt` timestamp NOT NULL,
-  `updatedAt` timestamp NOT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
   `createdBy` int NOT NULL DEFAULT '0',
-  `updatedBy` int NOT NULL DEFAULT '0',
+  `updatedBy` int DEFAULT NULL,
   `active` tinyint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `categories_createdby` (`createdBy`),
   KEY `categories_updatedby` (`updatedBy`),
   CONSTRAINT `categories_createdby` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`),
   CONSTRAINT `categories_updatedby` FOREIGN KEY (`updatedBy`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `images`
@@ -49,16 +60,28 @@ DROP TABLE IF EXISTS `images`;
 CREATE TABLE `images` (
   `id` int NOT NULL AUTO_INCREMENT,
   `filename` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL,
-  `url` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL,
+  `main` tinyint NOT NULL DEFAULT '0',
   `createdAt` timestamp NOT NULL,
   `updatedAt` timestamp NOT NULL,
   `createdBy` int NOT NULL,
   `active` tinyint NOT NULL,
+  `id_product` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `images_createdBy` (`createdBy`),
-  CONSTRAINT `images_createdBy` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  KEY `images_product` (`id_product`),
+  CONSTRAINT `images_createdBy` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`),
+  CONSTRAINT `images_product` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -83,15 +106,24 @@ CREATE TABLE `products` (
   `createdAt` timestamp NOT NULL,
   `updatedAt` timestamp NOT NULL,
   `createdBy` int NOT NULL,
-  `updatedBy` int NOT NULL,
+  `updatedBy` int DEFAULT NULL,
   `active` tinyint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `products_createdby` (`createdBy`),
   KEY `products_updatedby` (`updatedBy`),
   CONSTRAINT `products_createdby` FOREIGN KEY (`createdBy`) REFERENCES `users` (`id`),
   CONSTRAINT `products_updatedby` FOREIGN KEY (`updatedBy`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products_categories`
@@ -109,8 +141,17 @@ CREATE TABLE `products_categories` (
   KEY `category` (`categoryId`),
   CONSTRAINT `category` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `product` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_categories`
+--
+
+LOCK TABLES `products_categories` WRITE;
+/*!40000 ALTER TABLE `products_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products_categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products_images`
@@ -128,8 +169,17 @@ CREATE TABLE `products_images` (
   KEY `image_pdt` (`imageId`),
   CONSTRAINT `image_pdt` FOREIGN KEY (`imageId`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `product_img` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_images`
+--
+
+LOCK TABLES `products_images` WRITE;
+/*!40000 ALTER TABLE `products_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products_images` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -142,12 +192,22 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nickname` varchar(50) NOT NULL DEFAULT '0',
   `name` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(100) NOT NULL DEFAULT '0',
+  `password` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
   `role` int NOT NULL,
+  `active` tinyint NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nickname` (`nickname`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -158,4 +218,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-11 21:17:12
+-- Dump completed on 2022-07-24 16:47:28
